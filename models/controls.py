ac55de1ab6b32b5a923df1191a813bd7d62be13f
AndrewVasutenko <vasutenko@gmail.com>
2022-12-19 00:52:01 +0200

    init commit

---
+++ models/controls.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from selene.support.shared import browser
+from selenium.webdriver import Keys
+
+
+def select_gender_male():
+    gender_male = browser.element("#gender-radio-1").double_click()
+    gender_male.double_click()
+
+
+def select_date_of_birthday():
+    browser.element("#dateOfBirthInput").click()
+    browser.element(".react-datepicker__month-select").type("April")
+    browser.element(".react-datepicker__year-select").type("1993")
+    browser.element("[aria-label='Choose Monday, April 19th, 1993']").click()
+
+
+def fill_date_of_birthday(date):
+    name_os = sys.platform
+    if name_os == 'darwin':
+        browser.element("#dateOfBirthInput").send_keys(Keys.COMMAND + 'a').type(date).press_enter()
+    else:
+        browser.element("#dateOfBirthInput").send_keys(Keys.CONTROL + 'a').type(date).press_enter()
+
+
+def submit():
+    browser.element('#submit').press_enter()
+
+
+def select_hobby_music():
+    browser.element("[for=hobbies-checkbox-3]").click()
+
+
+def download_picture():
+    # browser.element("#uploadPicture").send_keys(os.path.abspath("../images/meme.jpg"))
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'resources/meme.jpg'))
+    )
+
+
+def select_state_and_city(state, city):
+    state_ = browser.element("#react-select-3-input")
+    state_.type(state).press_enter()
+    city_ = browser.element("#react-select-4-input")
+    city_.type(city).press_enter()
---
+++ tests/test_demoqa_practice_form.py
@@ -0,0 +1,44 @@
+import allure
+from selene import command
+from selene.support.shared import browser
+from models import pages
+from models import controls
+from selene.support.conditions import have
+
+
+@allure.title("Successful fill form")
+def test_demo_qa():
+
+    with allure.step("start demoqa"):
+        browser.open('/automation-practice-form')
+        ads = browser.all('[id^=google_ads][id$=container__]')
+        if ads.wait.until(have.size_less_than_or_equal(4)):
+            ads.perform(command.js.remove)
+
+    with allure.step("fill form"):
+        pages.fill_fullname("Andrew", "Vasutenko")
+        pages.fill_user_email("qqqqq@qqqq.qq")
+        controls.select_gender_male()
+        pages.fill_user_number_phone("8800555353")
+        controls.fill_date_of_birthday("18 December,2022")
+        pages.fill_subjects(("Art", "Commerce"))
+        controls.select_hobby_music()
+        controls.download_picture()
+        pages.fill_curent_adress("Russia,Moscow")
+        controls.select_state_and_city("Rajasthan", "Jaipur")
+        controls.submit()
+
+    with allure.step("check result"):
+        pages.table_result.should(have.text("Andrew"))
+        pages.table_result.should(have.text("Vasutenko"))
+        pages.table_result.should(have.text("qqqqq@qqqq.qq"))
+        pages.table_result.should(have.text("Male"))
+        pages.table_result.should(have.text("8800555353"))
+        pages.table_result.should(have.text("18 December,2022"))
+        pages.table_result.should(have.text("Arts, Commerce"))
+        pages.table_result.should(have.text("Music"))
+        pages.table_result.should(have.text("meme.jpg"))
+        pages.table_result.should(have.text("Russia,Moscow"))
+        pages.table_result.should(have.text("Rajasthan"))
+        pages.table_result.should(have.text("Jaipur"))
+        browser.element("#closeLargeModal").double_click()
